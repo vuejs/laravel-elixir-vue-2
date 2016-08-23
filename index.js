a430845e5744b276c386a2946c0bc1b35f51ab18
Evan You <yyx990803@gmail.com>
2016-08-23 11:20:49 +0200

    bump to use updated elixir webpack config method

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-var elixir = require('laravel-elixir')
+var Elixir = require('laravel-elixir')
 
-// temporary hack so that we can inject additional options to
-// laravel-exlixir-webpack-official
-var fn = elixir.mixins.webpack
-elixir.mixins.webpack = function (file, input, output, options) {
-  options = options || {}
-
-  options.resolve = options.resolve || {}
-  options.resolve.alias = options.resolve.alias || {}
-  options.resolve.alias.vue = 'vue/dist/vue.js'
-
-  options.vue = options.vue || {}
-  options.vue.loaders = options.vue.loaders || {}
-  options.vue.loaders.js = options.vue.loaders.js || 'buble'
-
-  return fn.call(this, file, input, output, options)
-}
-
-elixir.ready(function () {
-  elixir.config.js.webpack.loaders.push(
-    {
-      test: /\.vue$/,
-      loader: 'vue'
-    },
-    {
-      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-      loader: 'file',
-      query: {
-        limit: 10000,
-        name: '../img/[name].[hash:7].[ext]'
+Elixir.ready(function () {
+  Elixir.webpack.mergeConfig({
+    // ensure we are using the version of Vue that supports templates
+    resolve: {
+      alias: {
+        vue: 'vue/dist/vue.js'
       }
     },
-    {
-      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-      loader: 'url',
-      query: {
-        limit: 10000,
-        name: '../fonts/[name].[hash:7].[ext]'
+    // use buble loader since it is the default in Elixir
+    vue: {
+      loaders: {
+        js: 'buble-loader'
       }
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+          loader: 'file-loader',
+          query: {
+            limit: 10000,
+            name: '../img/[name].[hash:7].[ext]'
+          }
+        },
+        {
+          test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+          loader: 'url-loader',
+          query: {
+            limit: 10000,
+            name: '../fonts/[name].[hash:7].[ext]'
+          }
+        }
+      ]
     }
-  )
+  })
 })
